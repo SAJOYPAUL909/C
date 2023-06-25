a39eb083d61dc9c3303e78d7b5ad25ab1f09792c
SajoyPaul <sajoypaul909@gmail.com>
2023-06-25 23:25:12 +0530

    Q8 square of number

---
+++ Assignment1/square.c
@@ -0,0 +1,10 @@
+//square of user input number
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    printf("The Square of %d is %d",a,a*a);
+}
