47f4e8532799673acd3509aa9201e16c0d05a05b
SajoyPaul <sajoypaul909@gmail.com>
2023-06-26 22:33:49 +0530

    Q1 average of three number

---
+++ Assignment2/average.c
@@ -0,0 +1,19 @@
+// write a program to calculate average of three user input integer number
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a,b,c;
+    float average;
+    printf("Enter the first number :");
+    scanf("%d",&a);
+    printf("Enter the secound number :");
+    scanf("%d",&b);
+    printf("Enter the third number :");
+    scanf("%d",&c);
+
+    average = (a+b+c)/3.0;
+    printf("Average of %d,%d and %d is %f",a,b,c,average);
+
+}
