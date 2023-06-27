ee972501a72dbe5ee9168b9f1d34eabeac85dcbd
SajoyPaul <sajoypaul909@gmail.com>
2023-06-27 12:39:29 +0530

    Q7 ASCII to Character

---
+++ Assignment2/asciitochar.c
@@ -0,0 +1,14 @@
+//Write a program to input an ASCII code from the user and print its corresponding
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a;
+    printf("Enter the ASCII code :");
+    scanf("%d",&a);
+    printf("The Corresponding character to ASCII code %d is %c",a,a);
+
+
+}
