363a0e48558c0292cb1f3e6f95d7fd2381f269ae
SajoyPaul <sajoypaul909@gmail.com>
2023-06-28 15:11:35 +0530

    Q2. size of real constant

---
+++ Assignment3/sizeofrealconstant.c
@@ -0,0 +1,9 @@
+//Write a program to calculate size of a real constant.
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    float a = 234.5;
+    printf("Size of real constant : %d",sizeof(a));
+}
