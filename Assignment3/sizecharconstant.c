d8d25433c07b4ef62612a314bb70d89a3bdbb8b2
SajoyPaul <sajoypaul909@gmail.com>
2023-06-28 14:57:12 +0530

    Q1. size of character constant

---
+++ Assignment3/sizecharconstant.c
@@ -0,0 +1,9 @@
+//Write a program to calculate size of a character constant.
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    char a = 'A';
+    printf("Size of variable a is %d",sizeof(a));
+}
