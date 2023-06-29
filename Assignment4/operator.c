70782c5391763d044ea67e7b094a218f6d02e6e7
SajoyPaul <sajoypaul909@gmail.com>
2023-06-29 15:12:09 +0530

    Q8. x=10>8>4;

---
+++ Assignment4/operator.c
@@ -0,0 +1,10 @@
+//What will be the value stored in the variable x after executing following statement x=10>8>4;
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int x;
+    x = 10>8>4;
+    printf("X = %d",x);
+}
