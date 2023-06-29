0888b70bf03a6692fc5b0b561e5634b90eb058fe
SajoyPaul <sajoypaul909@gmail.com>
2023-06-29 15:28:38 +0530

    Q10 x=3<0885>0 Syntax error

---
+++ Assignment4/operator3.c
@@ -0,0 +1,17 @@
+//What will be the value stored in the variable x after executing following statement: x=3<0885>0;
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int x;
+    x=3<0885>0;
+    printf("X = %d",x);
+    /*The statement `x=3<0885>0;` is not valid in C. It will raise a syntax error. If you meant `x=3*8850;`, then the value stored in the variable x would be **26550**.
+
+Source: Conversation with Bing, 29/6/2023
+(1) Variables and Assignment — Python Numerical Methods. https://pythonnumericalmethods.berkeley.edu/notebooks/chapter02.01-Variables-and-Assignment.html.
+(2) (a) Write the value that will be assigned to variable x after executing .... https://www.sarthaks.com/208661/write-the-value-that-will-be-assigned-to-variable-after-executing-the-following-statement.
+(3) Incrementing in C++ - When to use x++ or ++x? - Stack Overflow. https://stackoverflow.com/questions/1812990/incrementing-in-c-when-to-use-x-or-x.
+(4) Solved What value will be stored in the variable x after - Chegg. https://www.chegg.com/homework-help/questions-and-answers/value-stored-variable-x-following-statements-executed-int-3-int-b-2-x-b-statement-1-x-2-st-q79018271.*/
+}
