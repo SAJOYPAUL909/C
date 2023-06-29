b17b58bd024ce28f19068f99aa1fb48914d356f0
SajoyPaul <sajoypaul909@gmail.com>
2023-06-29 14:39:38 +0530

    Q5.Last digit Append

---
+++ Assignment4/appenddigit.c
@@ -0,0 +1,16 @@
+//Write a program to input a number from the user and also input a digit. Append a digit in the number and print the resulting number. (Example-number-234 and digit 9 then the resulting number is 2349)
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a,d,n;
+    printf("Enter the number :");
+    scanf("%d",&a);
+
+    printf("Enter the digit :");
+    scanf("%d",&d);
+
+    n = (a*10 )+ d;
+    printf("Number after appending digit:%d",n);
+}
