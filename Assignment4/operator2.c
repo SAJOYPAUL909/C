5b7fd624a3cba79720b92cfb735a04f7e4b47383
SajoyPaul <sajoypaul909@gmail.com>
2023-06-29 15:21:26 +0530

    Q9. x=12x-2 error;

---
+++ Assignment4/operator2.c
@@ -0,0 +1,10 @@
+//What will be the value stored in the variable x after executing following statement. x=12x-2;
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int x;
+    x = 12*x-2;
+    printf("X = %d",x);
+}
