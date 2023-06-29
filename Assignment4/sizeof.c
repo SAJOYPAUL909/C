93c5578053eb50a5a09bfb685c9d6170474b17e2
SajoyPaul <sajoypaul909@gmail.com>
2023-06-29 14:27:12 +0530

    Q3.size of premitive data type

---
+++ Assignment4/sizeof.c
@@ -0,0 +1,9 @@
+//Write a program to print size of an int, a float, a char and a double type variable
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    printf("Size of :\nint = %d,\nfloat = %d,\nchar = %d,\ndouble = %d",sizeof(int),sizeof(float),sizeof(char),sizeof(double)); 
+}
