e5deba05f41db9a53d60452fd49f79a6800411b3
SajoyPaul <sajoypaul909@gmail.com>
2023-06-30 22:12:30 +0530

    Q10 .greatest of  three number

---
+++ Assignment5/greater2.c
@@ -0,0 +1,17 @@
+//Write a program to find the greatest among three given numbers. Print number once if the greatest number appears two or three times.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter three numbers :");
+    scanf("%d%d%d",&a,&b,&c);
+    if (a>=b && a>=c)
+    {
+        printf("%d is greatest",a);
+    }
+    else if (b>=a && b>=c)
+    printf("%d is greatest",b);
+    else
+    printf("%d is greatest",c);
+}
