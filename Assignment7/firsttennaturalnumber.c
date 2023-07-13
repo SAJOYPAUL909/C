fe9d26b15d4a87a4f72c3204e1c1e6c3d7754e66
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 17:57:04 +0530

    Q2. First 10 Natural Numbers

---
+++ Assignment7/firsttennaturalnumber.c
@@ -0,0 +1,13 @@
+//Write a program to print first 10 natural numbers
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int i;
+    for (i = 1; i<= 10;i++)
+    {
+        printf("%d ",i);
+    }
+}
