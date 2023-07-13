eca15763f4f90793f22b50edc75383bdefaf6d99
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 20:13:39 +0530

    Q9. First 10 Natural Numbers Cubes

---
+++ Assignment7/firsttennumbercubes.c
@@ -0,0 +1,11 @@
+//write a program to print cubes of the 10 natural number
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i;
+    for (i = 1;i<=10;i++ )
+    {
+        printf("%d ",i*i*i);
+    }
+}
