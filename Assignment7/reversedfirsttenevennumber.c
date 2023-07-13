c5f5be188b3a5a0dc33ccec54b600d953c2d543e
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 20:03:40 +0530

    Q7. First 10 even Natural Numbers in Reversed Order

---
+++ Assignment7/reversedfirsttenevennumber.c
@@ -0,0 +1,16 @@
+////write a program to print first 10 even natural number in reversed order.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i ;
+    
+    for (i= 10*2 ;i > 0;i--)
+    {
+        if (i%2 == 0)
+        {
+            printf("%d ",i);
+        }
+    }
+
+}
