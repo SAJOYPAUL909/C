19a2d737eb47638992d1ca8aa3b6ee82467620c8
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 20:09:44 +0530

    Q8. First 10 Natural Numbers Squares

---
+++ Assignment7/firsttennumbersquare.c
@@ -0,0 +1,11 @@
+//write a program to print squares of the first 10 natural numbers
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i ;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d ",i*i);
+    }
+}
