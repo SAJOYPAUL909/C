6a435c733b4c2a99c8303d05ce3d1cd528d7b58d
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 17:49:52 +0530

    Print Hello Five Times on Screen

---
+++ Assignment7/fivetimes.c
@@ -0,0 +1,13 @@
+// Write a program to print Hello five times on the Screen
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int i ;
+    for (i =0 ; i < 5; i++)
+    {
+        printf("Hello ");
+    }
+}
