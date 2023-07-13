1539087b6cf8c2b3c4003ca124dc86552a2239e6
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 18:55:46 +0530

    Q4. First 10 odd Natural Numbers

---
+++ Assignment7/firsttenoddnumber.c
@@ -0,0 +1,17 @@
+//write a program to print first 10 odd natural number
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i = 0;
+    int j = 1;
+    while (i < 10)
+    {
+        if (j %2 !=0)
+        {
+            printf("%d ",j);
+            i++;
+        }
+        j++;
+    }
+}
