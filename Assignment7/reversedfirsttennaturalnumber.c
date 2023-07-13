e04f9d79b39e344a28f8049e2cc852d19da97004
SajoyPaul <sajoypaul909@gmail.com>
2023-07-13 18:46:10 +0530

    Q3. First 10 Natural Numbers in Reversed Order

---
+++ Assignment7/reversedfirsttennaturalnumber.c
@@ -0,0 +1,11 @@
+////Write a program to print first 10 natural numbers in reversed order
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i ;
+    for (i = 10 ; i > 0 ;i--)
+    {
+        printf("%d ",i);
+    }
+}
