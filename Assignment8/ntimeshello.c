ae040f8340c15d67030f43fa061cf863827d908c
SajoyPaul <sajoypaul909@gmail.com>
2023-07-15 20:27:07 +0530

    Q1. Print Hello N Times on Screen

---
+++ Assignment8/ntimeshello.c
@@ -0,0 +1,15 @@
+//Write a program to print MySirG N times on the screen
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,i;
+    printf("Enter N ");
+    scanf ("%d",&n);
+
+    for (i = 0 ; i < n ; i++)
+    {
+        printf("Hello ");
+    }
+
+}
