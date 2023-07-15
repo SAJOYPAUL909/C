08f7f9e8f46acd2f11059b271c7ca0bfadce07eb
SajoyPaul <sajoypaul909@gmail.com>
2023-07-15 22:15:02 +0530

    Q8. First N Natural Numbers Squares

---
+++ Assignment8/firstnnumbersquare.c
@@ -0,0 +1,14 @@
+//Write a program to print squares of the first N natural numbers
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i,n ;
+    printf("Enter N : ");
+    scanf("%d",&n);
+    printf("First N natural number squares : ");
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ",i*i);
+    }
+}
