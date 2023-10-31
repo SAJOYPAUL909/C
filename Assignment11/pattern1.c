81df46126a0bbe1eba0c8f0ffa1d034f852c5793
SajoyPaul <sajoypaul909@gmail.com>
2023-10-31 16:33:21 +0530

    Q1. Pattern-1

---
+++ Assignment11/pattern1.c
@@ -0,0 +1,26 @@
+// Write  a program to draw the following patterns:
+//  *
+//  **
+//  ***
+//  ****
+//  *****
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i,j,n;
+
+    printf("Enter the no.s of Line for Pattern 1 :  ");
+    scanf("%d",&n);
+
+    for (i = 1 ; i <=n ;i++)
+    {
+        for (j = 1 ; j <= i ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+}
