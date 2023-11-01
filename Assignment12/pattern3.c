52b171fa60a218e0e2e98ce60b33619f57bd4412
SajoyPaul <sajoypaul909@gmail.com>
2023-11-01 17:06:00 +0530

    Q3. Pattern-3

---
+++ Assignment12/pattern3.c
@@ -0,0 +1,30 @@
+// Write  a program to draw the following patterns:
+//    *
+//   * *
+//  * * *
+// * * * *
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i,j,k;
+    int n;
+
+    printf("Enter the no.s of Line for Pattern 3 :  ");
+    scanf("%d",&n);
+
+    for (i = 1 ; i <= n ; i++)
+    {
+        for (j =  n ; j >= i ; j-- )
+        {
+            printf(" ");
+        }
+        for (k = 1 ; k <= i ; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+}
