452917f6491001bfc63a94535523698e6245e171
SajoyPaul <sajoypaul909@gmail.com>
2023-11-03 22:02:57 +0530

    Q8. Pattern-8

---
+++ Assignment12/pattern8.c
@@ -0,0 +1,36 @@
+// Write  a program to draw the following patterns:
+// *******
+// *** ***
+// **   **
+// *     *
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+
+{
+    int i,j,k,l,m;
+    int n;
+
+    printf("Enter the no.s of Line for Pattern 8 :  ");
+    scanf("%d",&n);
+
+    for (i = 0; i < n ; i++)
+    {
+        for (j = -n +1; j < n ; j++)
+        {
+            if (j < i && -i < j )
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        
+
+        
+        printf("\n");
+    }
+}
